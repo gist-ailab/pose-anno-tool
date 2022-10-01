43791179421166908f44c4e5c3e6f510e6bbc539
seungback <shback@gist.ac.kr>
2022-10-01 21:32:37 +0900

    fix: Errors on load rgb-d

--- gt_visualizer/data2_gt_visualizer.py
+++ gt_visualizer/data2_gt_visualizer.py
@@ -1,4 +1,4 @@
-import imgviz
+from fileinput import filename
 import cv2
 import numpy as np
 import os
@@ -27,10 +27,13 @@ class GTVisualizer():
         self.width, self.height = 1920, 720
         self.frame = np.zeros((self.height, self.width, 3), dtype=np.uint8)
         self.is_updated = True
+        self.rgb_path = None
+        self.depth_path = None
 
         aihub_root = input("실제 / 가상 폴더의 경로를 입력해주세요: \n")
+        # aihub_root = "/home/seung/OccludedObjectDataset/aihub/원천데이터/다수물체가림/실제"
         self.aihub_root = aihub_root
-        self.scene_id = 1
+        self.scene_id = 51
         self.get_sub_dirs_from_scene_id()
         self.image_id = 1
 
@@ -38,6 +41,7 @@ class GTVisualizer():
         self.on_scene_id(self.scene_id)
         self.on_image_id(self.image_id)
         self.black = np.zeros((self.height//2, self.width//3, 3), dtype=np.uint8)
+
 
     def get_sub_dirs_from_scene_id(self):
         
@@ -51,6 +55,13 @@ class GTVisualizer():
                         return
         print("존재하지 않는 scene_id가 입력되었습니다.")
         
+    def get_filename_from_image_id(self):
+        if not os.path.exists(os.path.join(self.scene_path, "rgb")):
+            return None
+        for file_name in os.listdir(os.path.join(self.scene_path, "rgb")):
+            if int(file_name.split("_")[-1].split(".")[0]) == self.image_id:
+                return file_name
+
 
 
     def init_cv2(self):
@@ -64,39 +75,45 @@ class GTVisualizer():
         self.scene_id = val
         self.get_sub_dirs_from_scene_id()
         self.scene_path = os.path.join(self.aihub_root, self.sub_dir_1, self.sub_dir_2, "{0:06d}".format(self.scene_id))
-        self.is_updated = False
         self.on_image_id(self.image_id)
+        self.is_updated = False
         cv2.setTrackbarPos('scene_id','GIST AILAB Data2 GT Visualizer', self.scene_id)
 
     def on_image_id(self, val):
         self.image_id = val
-        self.rgb_path = os.path.join(self.scene_path, "rgb", "{0:06d}.png".format(self.image_id))
-        self.depth_path = os.path.join(self.scene_path, "depth", "{0:06d}.png".format(self.image_id))
-        self.gt_path = os.path.join(self.scene_path, "gt", "{0:06d}.json".format(self.image_id))
+        cv2.setTrackbarPos('image_id','GIST AILAB Data2 GT Visualizer', self.image_id)
+        file_name = self.get_filename_from_image_id()
+        if file_name is None:
+            return
+        self.rgb_path = os.path.join(self.scene_path, "rgb", file_name)
+        self.depth_path = os.path.join(self.scene_path, "depth", file_name)
+        if ".jpg" in self.depth_path:
+            self.depth_path = self.depth_path.replace(".jpg", ".png")
+        self.gt_path = os.path.join(self.scene_path, "gt", file_name.split(".")[0] + ".json")
         self.is_updated = False
         cv2.setTrackbarPos('image_id','GIST AILAB Data2 GT Visualizer', self.image_id)
 
+
     def update_vis(self):
-        if not self.is_updated:
-            try:
-                self.rgb, self.depth = self.load_rgbd()
-                self.amodal, self.vis, self.occ = self.visualize_masks()
-            except FileNotFoundError:
-                pass
+        if not self.is_updated and self.rgb_path is not None:
+            self.rgb, self.depth = self.load_rgbd()
+            self.amodal, self.vis, self.occ = self.visualize_masks()
             self.rgb = cv2.putText(self.rgb, "RGB", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             self.depth = cv2.putText(self.depth, "DEPTH", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             self.amodal = cv2.putText(self.amodal, "AMODAL MASK", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             self.vis = cv2.putText(self.vis, "VISIBLE MASK", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             self.occ = cv2.putText(self.occ, "INVISIBLE MASK", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             self.is_updated = True
-        rgbd = np.hstack((self.rgb, self.depth, self.black))
-        masks = np.hstack((self.amodal, self.vis, self.occ))
-        self.frame = np.vstack((rgbd, masks))
+            rgbd = np.hstack((self.rgb, self.depth, self.black))
+            masks = np.hstack((self.amodal, self.vis, self.occ))
+            self.frame = np.vstack((rgbd, masks))
+        
 
         
     def load_rgbd(self):
-        
+
         rgb = read_image(self.rgb_path)
+        self.size = list(rgb.shape[:2] )
         depth = read_image(self.depth_path)
         depth = depth.astype(np.float32)
         min_depth = np.unique(np.partition(depth.flatten(), 1))[1]
@@ -109,6 +126,7 @@ class GTVisualizer():
         depth = cv2.resize(depth, (self.width//3, self.height//2), interpolation=cv2.INTER_NEAREST)
         return rgb, depth
 
+
     def visualize_masks(self):
         
         self.gt = json.load(open(self.gt_path, "r"))
@@ -120,7 +138,7 @@ class GTVisualizer():
         vis_toplefts = []
         occ_toplefts = []
         for idx, anno in enumerate(self.annotations):
-            amodal_mask = M.decode(anno["amodal_mask"])
+            amodal_mask = M.decode({'counts': anno["amodal_mask"], 'size': self.size})
             amodal_mask = amodal_mask.astype(np.uint8)
             amodal_mask = cv2.resize(amodal_mask, (self.width//3, self.height//2), interpolation=cv2.INTER_NEAREST)
             amodal_mask = amodal_mask.astype(bool)
@@ -132,7 +150,7 @@ class GTVisualizer():
             x, y = x1 + (x2 - x1) // 2, y1 + (y2 - y1) // 2
             amodal_toplefts.append((y, x))
 
-            vis_mask = M.decode(anno["visible_mask"])
+            vis_mask = M.decode({'counts': anno["visible_mask"], 'size': self.size})
             vis_mask = vis_mask.astype(np.uint8)
             vis_mask = cv2.resize(vis_mask, (self.width//3, self.height//2), interpolation=cv2.INTER_NEAREST)
             vis_mask = vis_mask.astype(bool)
@@ -144,12 +162,11 @@ class GTVisualizer():
             x, y = x1 + (x2 - x1) // 2, y1 + (y2 - y1) // 2
             vis_toplefts.append((y, x))
 
-            occ_mask = M.decode(anno["invisible_mask"])
+            occ_mask = M.decode({'counts': anno["invisible_mask"], 'size': self.size})
             occ_mask = occ_mask.astype(np.uint8)
             occ_mask = cv2.resize(occ_mask, (self.width//3, self.height//2), interpolation=cv2.INTER_NEAREST)
             occ_mask = occ_mask.astype(bool)
             occ_masks.append(occ_mask)
-
             x, y = np.where(occ_mask)
             try:
                 x1, y1 = x.min(), y.min()
@@ -158,8 +175,6 @@ class GTVisualizer():
             except:
                 x, y = 0, 0
             occ_toplefts.append((y, x))
-
-
 
         # draw amodal and visible masks on rgb
         amodal = self.rgb.copy()
